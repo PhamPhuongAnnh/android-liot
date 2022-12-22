3cfc659dc88eb64bfff4982519b76ac6ab99c378
Pham Van Dung <dung18j@gmail.com>
2022-12-22 18:38:16 +0700

    init4

--- app/src/main/java/com/example/android_liot/MainActivity.java
+++ app/src/main/java/com/example/android_liot/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.android_liot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,36 +29,63 @@ import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
     Stack<JSONObject> properties = new Stack<>();
-    ArrayList<Device> devices = new ArrayList<>();
+    ArrayList<String> device_ips = new ArrayList<>();
+    String selectedIp;
+    //ArrayList<String> device_names = new ArrayList<>();
+
 
     LinearLayout all_device_layout;
     LinearLayout detail_device_layout;
     Button btnBack;
-    HttpService controller;
-
+    RequestQueue mQueue;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        controller = new HttpService(this);
+        context = this;
+        mQueue = Volley.newRequestQueue(this);
+        device_ips.add("192.168.71.1");
 
         setContentView(R.layout.activity_main);
         all_device_layout = findViewById(R.id.all_device_layout);
         detail_device_layout = findViewById(R.id.detail_device_layout);
 
 
-        View view = getLayoutInflater().inflate(R.layout.device_card, null);
-        TextView name1 = view.findViewById(R.id.device_name);
-        name1.setText("Device 1");
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                findViewById(R.id.all_device_layout_parent).setVisibility(View.GONE);
-                findViewById(R.id.detail_device_layout_parent).setVisibility(View.VISIBLE);
-                jsonParse();
-                showDetail();
-            }
-        });
-        all_device_layout.addView(view);
+        for (int i = 0; i < device_ips.size(); i++) {
+            selectedIp = device_ips.get(i);
+            View view = getLayoutInflater().inflate(R.layout.device_card, null);
+            TextView name1 = view.findViewById(R.id.device_name);
+            name1.setText("Device 1");
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, "http://" + selectedIp + "/schema", null, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                response.put("type", "object");
+                                properties.clear();
+                                properties.push(response.getJSONObject("properties"));
+                                findViewById(R.id.all_device_layout_parent).setVisibility(View.GONE);
+                                findViewById(R.id.detail_device_layout_parent).setVisibility(View.VISIBLE);
+
+                                showDetail();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("log", "onErrorResponse: ", error);
+                        }
+                    });
+                    mQueue.add(request);
+                }
+            });
+            all_device_layout.addView(view);
+        }
     }
 
     @Override
@@ -66,53 +103,9 @@ public class MainActivity extends AppCompatActivity {
     void viewAllDevice() {
 
     }
+
     void viewDevice() {
 
-    }
-    View objectToView(JSONObject field) {
-        Log.i("log", field.toString());
-        try {
-            String id_str = field.getString("id");
-            String title_str = field.getString("title");
-            String obj_type = field.getString("type");
-            if (obj_type.equals("bool")) {
-                View view = getLayoutInflater().inflate(R.layout.layout_bool, null);
-                TextView title = view.findViewById(R.id.title_bool);
-                Switch switch1 = view.findViewById(R.id.switch_bool);
-                title.setText(title_str);
-                return view;
-            } else if (obj_type.equals("integer")) {
-                View view = getLayoutInflater().inflate(R.layout.layout_integer, null);
-                TextView title = view.findViewById(R.id.title_integer);
-                title.setText(title_str);
-                return view;
-            } else if (obj_type.equals("object")) {
-                View view = getLayoutInflater().inflate(R.layout.layout_object, null);
-                view.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        try {
-                            properties.push(field.getJSONObject("properties"));
-                            showDetail();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-                TextView title = view.findViewById(R.id.title_object);
-                title.setText(title_str);
-                return view;
-            } else {
-                View view = getLayoutInflater().inflate(R.layout.layout_text, null);
-                TextView title = view.findViewById(R.id.title_text);
-                title.setText(title_str);
-                return view;
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        View view = getLayoutInflater().inflate(R.layout.layout_text, null);
-        return view;
     }
 
     public void showDetail() {
@@ -123,8 +116,95 @@ public class MainActivity extends AppCompatActivity {
             while (keys.hasNext()) {
                 String key = keys.next();
                 JSONObject field = current.getJSONObject(key);
-                View field_view = objectToView(field);
-                detail_device_layout.addView(field_view);
+                View view = null;
+
+                String id_str = field.getString("id");
+                String title_str = field.getString("title");
+                String obj_type = field.getString("type");
+
+                if (obj_type.equals("bool")) {
+                    view = getLayoutInflater().inflate(R.layout.layout_bool, null);
+                    TextView title = view.findViewById(R.id.title_bool);
+                    Switch switch1 = view.findViewById(R.id.switch_bool);
+                    title.setText(title_str);
+                    switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                            JSONObject obj = new JSONObject();
+                            try {
+                                obj.put(id_str, b);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, "http://" + selectedIp + "/data",obj, new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Log.e("log", "onErrorResponse: ", error);
+                                }
+                            });
+                            mQueue.add(request);
+                        }
+                    });
+                    detail_device_layout.addView(view);
+                } else if (obj_type.equals("integer")) {
+                    view = getLayoutInflater().inflate(R.layout.layout_integer, null);
+                    TextView title = view.findViewById(R.id.title_integer);
+                    title.setText(title_str);
+                    detail_device_layout.addView(view);
+                } else if (obj_type.equals("object")) {
+                    view = getLayoutInflater().inflate(R.layout.layout_object, null);
+                    view.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            try {
+                                properties.push(field.getJSONObject("properties"));
+                                showDetail();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                    TextView title = view.findViewById(R.id.title_object);
+                    title.setText(title_str);
+                    detail_device_layout.addView(view);
+                } else {
+                    view = getLayoutInflater().inflate(R.layout.layout_text, null);
+                    TextView title = view.findViewById(R.id.title_text);
+                    title.setText(title_str);
+                    TextView textView = view.findViewById(R.id.text_field_text);
+                    view.findViewById(R.id.text_field_send).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            String content = textView.getText().toString();
+                            JSONObject obj = new JSONObject();
+                            try {
+                                obj.put(id_str, content);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, "http://" + selectedIp + "/data",obj, new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Toast.makeText(context, "Request SEnt", Toast.LENGTH_SHORT).show();
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Log.e("log", "onErrorResponse: ", error);
+                                }
+                            });
+                            mQueue.add(request);
+
+                        }
+                    });
+                    detail_device_layout.addView(view);
+                }
             }
         } catch (JSONException e) {
             e.printStackTrace();
